6b3fe71b76a0c11b071d2feba3a34ada3c429e96
s24468 <s24468@pjwstk.edu.pl>
2023-04-14 00:20:50 +0200

    Initial Chart Animation

--- PSM_6/MainWindow.xaml.cs
+++ PSM_6/MainWindow.xaml.cs
@@ -1,31 +1,74 @@
-﻿using LiveCharts;
+﻿using System;
+using System.Windows.Threading;
+using LiveCharts;
 using LiveCharts.Defaults;
 using LiveCharts.Wpf;
+
 
 namespace PSM_6
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow
     {
+        private readonly DispatcherTimer _timer;
+
+        // private ChartValues<ObservablePoint> chart;
+        private ChartValues<double> chart;
+        private LineSeries lineSeries;
+
         public MainWindow()
         {
             InitializeComponent();
             //tworzenie wykresu
-            var chart = new ChartValues<ObservablePoint>();
-            var lineSeries = new LineSeries()
+            // chart = new ChartValues<ObservablePoint>()
+            //     { new ObservablePoint(1, 1), new ObservablePoint(2, 2), new ObservablePoint(3, 3) };
+            chart = new ChartValues<double>()
+                { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+
+            lineSeries = new LineSeries()
             {
                 Title = "Moon",
                 Values = chart,
                 PointGeometry = null // Usuwa punkty z wykresu
             };
+
             var serial = new SeriesCollection() { lineSeries };
             var cartesianChart = new CartesianChart()
             {
                 Series = serial,
             };
             Content = cartesianChart;
+
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // // Dodaj nowy punkt do wykresu.
+            // chart.Add(chart.Count);
+            //
+            // // Usuń stary punkt z wykresu, jeśli chcesz ograniczyć liczbę widocznych punktów.
+            // if (chart.Count > 10)
+            // {
+            //     chart.RemoveAt(0);
+            // }
+            for (int i = 0; i < chart.Count; i++)
+            {
+                if (i % 2 == 0 &&chart[i]<=9)
+                {
+                    chart[i]++;
+                }
+                else if (i % 2 == 0)
+                {
+                    chart[i]--;
+                }
+                
+            }
         }
     }
 }
