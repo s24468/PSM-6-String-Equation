e5cace9cda3c143efb935afc316caf40ec7fbb4e
s24468 <s24468@pjwstk.edu.pl>
2023-04-13 23:44:17 +0200

    initial chart commiy

---
+++ PSM_6/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace PSM_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //tworzenie wykresu
+            var chart = new ChartValues<ObservablePoint>();
+            var lineSeries = new LineSeries()
+            {
+                Title = "Moon",
+                Values = chart,
+                PointGeometry = null // Usuwa punkty z wykresu
+            };
+            var serial = new SeriesCollection() { lineSeries };
+            var cartesianChart = new CartesianChart()
+            {
+                Series = serial,
+            };
+            Content = cartesianChart;
+        }
+    }
+}
